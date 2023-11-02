ef4699b9a5967a4e9138c9325feed5baadebc91d
zarigata <Ferrary@12>
2023-11-02 01:28:31 -0300

    Python is better

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Python e muito melhor...");
+        System.out.println("Esse aplicativo seria mais legal em python");
+        //DECLARACAO DO FILME
+        System.out.println("Tanque: Panzerkrafwagen Aufs IV : Tiger");
+        int yearOfLauch = 1942;//declaracao de integro
+        System.out.println("Debunqued:"+yearOfLauch);
+        boolean incluidoNoPlano = false;
+        double battleRatting = 6.3;
     }
 }
