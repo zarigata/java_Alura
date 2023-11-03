c7e6176312c2a8b04848b53ec26cf2d23445ed2c
zarigata <Ferrary@12>
2023-11-03 17:08:17 -0300

    Python is better

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Esse aplicativo seria mais legal em python");
         //DECLARACAO DO FILME
-        System.out.println("Tanque: Panzerkrafwagen Aufs IV : Tiger");
+        System.out.println("Tank: Panzerkrafwagen Aufs VI : Tiger");
         int yearOfLauch = 1942;//declaracao de integro
         System.out.println("Debunqued:"+yearOfLauch);
         boolean incluidoNoPlano = false;
         double battleRatting = 6.4;
+        String about;
+        about = """
+        Tiger is the most famous tank in ww2 of the german army
+        made by Heinkel and powered by porche, this tank was the most feared and most destructible in the fields of ww2 in the 1942 era
+        
+        """;
+        System.out.println(about);
     }
 }
